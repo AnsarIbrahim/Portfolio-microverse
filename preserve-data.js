023340726e1749c0e53adca2c226a8a70adbbcca
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-03-12 12:23:48 +0530

    Local-storage

--- preserve-data.js
+++ preserve-data.js
@@ -1,22 +1,23 @@
+const form = document.getElementById('form');
 const userName = document.getElementById('name');
 const userEmail = document.getElementById('email');
 const userMsg = document.getElementById('message');
 
 form.addEventListener('change', () => {
-    const userData = {
-        name: userName.value,
-        email: userEmail.value,
-        message:userMsg.value,
-    };
+  const userData = {
+    name: userName.value,
+    email: userEmail.value,
+    message: userMsg.value,
+  };
 
-    const userDataSerialized = JSON.stringify(userData);
-    localStorage.setItem('userData', userDataSerialized);
+  const userDataSerialized = JSON.stringify(userData);
+  localStorage.setItem('userData', userDataSerialized);
 });
 
 window.addEventListener('load', () => {
-    const userDataSerialized = JSON.parse(localStorage.userData);
+  const userDataSerialized = JSON.parse(localStorage.userData);
 
-    userName.value = userDataSerialized.name;
-    userEmail.value = userDataSerialized.email;
-    userMsg.value = userDataSerialized.message;
+  userName.value = userDataSerialized.name;
+  userEmail.value = userDataSerialized.email;
+  userMsg.value = userDataSerialized.message;
 });
